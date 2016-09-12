64ac052bdc493ee8079759ab49a5e9f5e1536830
Jimmyya <812752853@qq.com>
2016-09-12 14:56:53 +0800

    上传文件

--- simple_node/file_upload.js
+++ simple_node/file_upload.js
@@ -14,8 +14,8 @@ var server=http.createServer(function (req,res) {
 });
 var fs=require('fs');
 function show(req,res) {
-    var html=fs.readFileSync('file_upload.html','utf-8');
-    console.log(html);
+    var html=fs.readFileSync('file_upload.html','utf-8');//同步的方式读取
+    //console.log(html);
     res.setHeader('Content-Type','text/html;charset="utf-8"');
     res.setHeader('Content-Length',Buffer.byteLength(html));
     res.end(html);
@@ -25,6 +25,7 @@ function show(req,res) {
 
 var formidable=require('formidable');
 function isFormData(req) {
+    //检查是否以multipart/form-data开头
     var type=req.headers['content-type']||'';
     return 0==type.indexOf('multipart/form-data');
 }
@@ -34,6 +35,22 @@ function upload(req,res) {
         res.end('Bad Request');
         return ;
     }
+    var form=new formidable.IncomingForm();
+    form.parse(req);//解析data
+
+    form.on('field',function (field,value) {
+        console.log(field);
+        console.log(value);
+    });
+    form.on('file',function (name,file) {
+        console.log(name);
+        console.log(file);
+    });
+    form.on('end',function () {
+        res.end('upload complete');
+    });
+    form.parse(req);
+
 }
 
 server.listen(3000);
