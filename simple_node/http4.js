b216787b18a291514cc073dd7c9dddee49eb4e49
Jimmyya <812752853@qq.com>
2016-11-20 15:26:55 +0800

    创建文件通道

--- simple_node/http4.js
+++ simple_node/http4.js
@@ -1,5 +1,6 @@
 /**
  * Created by CHEN on 2016/9/11.
+ * 相比起http3.js 多了一个错误捕捉 防止 访问路径不存在报错
  */
 var http=require('http')
 var parse =require('url').parse;
@@ -8,13 +9,14 @@ var fs=require('fs');
 
 var root =__dirname;
 
-var server=http.createServer(function (req,res) {
+var server=http.createServer(function (req,res) {//request、response
     var url=parse(req.url);
     var path=join(root,url.path);
-    var stream=fs.createReadStream(path);
-    stream.pipe(res);
+    var stream=fs.createReadStream(path);//创建文件通道
+    stream.pipe(res);//stream通道传输给response
     //在Node中，所有继承EventEmitter的类都可能发生error事件，
     //为了防止服务器被搞垮，我们要监听错误
+    //http://localhost:3000/hello.html
     stream.on('error',function (err) {
         res.statusCode=500;
         res.end('Inrernal server error');
